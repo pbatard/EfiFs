90e306985ec7c4006d0c34bff9d9133ee3de0489
Pete Batard <pete@akeo.ie>
2014-12-12 18:34:37 +0000

    added ext2/ext3/ext3 driver for MSVC

---
+++ .msvc/newfs.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This script creates a new driver MSVC project
+TEMPLATE=exfat
+TEMPLATE_GUID=25E5B551-F9DD-4D25-A7CD-A1090B558A49
+GUID=`curl -s http://createguid.com | grep textarea | sed 's/.*<textarea.*>\(.*\)<\/textarea.*/\1/'`
+cp $TEMPLATE.vcxproj $1.vcxproj
+cp $TEMPLATE.vcxproj.filters $1.vcxproj.filters
+cp $TEMPLATE.vcxproj.user $1.vcxproj.user
+sed -i "s/$TEMPLATE_GUID/$GUID/" $1.vcxproj
+sed -i "s/$TEMPLATE/$1/g" $1.vcxproj
+sed -i "s/$/\r/" $1.vcxproj
+sed -i "s/$TEMPLATE/$1/g" $1.vcxproj.filters
+sed -i "s/$/\r/" $1.vcxproj.filters
+echo Created project files using GUID $GUID
