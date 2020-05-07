403ccf355dcb2f6f38fbc810f6e2a7f7fe4a2b48
Pete Batard <pete@akeo.ie>
2020-05-07 17:52:00 +0100

    fix a damaging typo in the _release.cmd script
    * The IA32 drivers were picked from the AARCH64 built folder with
      the expected disastrous results. Fix that.
    * Addresses a small part of issue #20

--- _release.cmd
+++ _release.cmd
@@ -10,7 +10,7 @@ mkdir release\arm
 for /f %%f in ('dir /b %EDK2_BUILD%\AARCH64\*.efi') do copy %EDK2_BUILD%\ARM\%%f release\arm\%%~nf_arm%%~xf
 
 mkdir release\ia32
-for /f %%f in ('dir /b %EDK2_BUILD%\IA32\*.efi') do copy %EDK2_BUILD%\AARCH64\%%f release\ia32\%%~nf_ia32%%~xf
+for /f %%f in ('dir /b %EDK2_BUILD%\IA32\*.efi') do copy %EDK2_BUILD%\IA32\%%f release\ia32\%%~nf_ia32%%~xf
 
 mkdir release\x64
 for /f %%f in ('dir /b %EDK2_BUILD%\X64\*.efi') do copy %EDK2_BUILD%\X64\%%f release\x64\%%~nf_x64%%~xf
